22489878b775e0324a00b1bf41efc3de0bdf3acc
JhonnSantiagoBernalCampus <jhonbernalcampus@gmail.com>
2023-03-10 09:00:16 -0500

    feat: :sparkles: Ejercicio 6

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-let num = Number(prompt("Ingrese el primer numero"));
-let num2 = Number(prompt("Ingrese el segundo numero"));
-if (num > num2) {
-    let suma = num + num2;
-    let diferencia = num - num2;
-    alert(`La suma de los numeros ${num} y ${num2} = ${suma}\nY su diferencia es de ${diferencia}`);
+let cantidad = Number(prompt("Ingrese la cantidad de estudiantes que desea ingresar"));
+let nombres = [];
+let masculino = [];
+let femenino = [];
+let notaFinal = [];
+for (let i = 0; i < cantidad; i++) {
+    nombres.push(prompt("Ingrese el nombre"));
+    decision = prompt("Ingrese el sexo\n\t1.Masculino\n\t2.Femenino");
+    if (decision == 1) {
+        masculino.push(decision);
+    }
+    else {
+        femenino.push(decision);
+    }
+    notaFinal.push(Number(prompt("Ingrese la nota final")));
 }
-else {
-    let producto = num * num2;
-    let division = num2 / num;
-    alert(`El producto de los numeros ${num} y ${num2} = ${producto}\nY su division es ${division}`);
-}
+
+let indiceMayor = notaFinal.indexOf(Math.max(...notaFinal));
+let indiceMenor = notaFinal.indexOf(Math.min(...notaFinal));
+let cantidadM = masculino.length;
+let cantidadF = femenino.length;
+alert(`La persona con la nota mayor es ${nombres[indiceMayor]}\n\tCon una nota final de ${notaFinal[indiceMayor]}\nLa persona con la nota menor es ${nombres[indiceMenor]}\n\tCon una nota final de ${notaFinal[indiceMenor]}\nLa cantidad de personas con sexo masculino es ${cantidadM}\nLa cantadidad de personas con sexo femenino es ${cantidadF}`);
