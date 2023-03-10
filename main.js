978c173abd95aa613b28074c9ac3e7a59a56170b
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-10 12:58:31 -0500

    feat: :sparkles: Ejercicio 7

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
-let cantidad = Number(prompt("Ingrese la cantidad de estudiantes que desea ingresar"));
-let nombres = [];
-let masculino = [];
-let femenino = [];
-let notaFinal = [];
-for (let i = 0; i < cantidad; i++) {
-    nombres.push(prompt("Ingrese el nombre"));
-    decision = prompt("Ingrese el sexo\n\t1.Masculino\n\t2.Femenino");
-    if (decision == 1) {
-        masculino.push(decision);
-    }
-    else {
-        femenino.push(decision);
-    }
-    notaFinal.push(Number(prompt("Ingrese la nota final")));
-}
-
-let indiceMayor = notaFinal.indexOf(Math.max(...notaFinal));
-let indiceMenor = notaFinal.indexOf(Math.min(...notaFinal));
-let cantidadM = masculino.length;
-let cantidadF = femenino.length;
-alert(`La persona con la nota mayor es ${nombres[indiceMayor]}\n\tCon una nota final de ${notaFinal[indiceMayor]}\nLa persona con la nota menor es ${nombres[indiceMenor]}\n\tCon una nota final de ${notaFinal[indiceMenor]}\nLa cantidad de personas con sexo masculino es ${cantidadM}\nLa cantadidad de personas con sexo femenino es ${cantidadF}`);
+let nombre = prompt("Ingrese el nombre del articulo");
+let precio = Number(prompt("Ingrese el valor del producto"))
+let cantidad = Number(prompt("Ingrese la cantidad que desea comprar"))
+alert(` - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n
+\tArticulo:\t\t${nombre}\n
+\tPrecio c/u:\t\t${precio} $\n
+\tCantidad:\t\t${cantidad}\n
+\tPrecio total:\t${(precio*cantidad)} $\n
+\tMuchas gracias por su compra\n
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -`)
