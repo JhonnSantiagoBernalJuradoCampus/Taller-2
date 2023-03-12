5f5355457abaeb0d6f8ffef731cbfa2865054d90
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-11 20:09:36 -0500

    feat: :sparkles: Ejercicio 9

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-let lado = Number(prompt("Ingrese el lado del cuadrado en cm"));
-let base = Number(prompt("Ingrese la base del rectangulo en cm"));
-let altura = Number(prompt("Ingrese la altura del rectangulo en cm"));
-let perimetro = (lado*4)
-let areaR = (base*altura)
-alert(`El perimetro del cuadradro es ${perimetro} cm\nEl area del rectangulo es ${areaR} cm`);
+let finalistas = 3;
+let nombres = [];
+let marcas = [];
+
+for (let i = 0; i < finalistas; i++) {
+    nombres.push(prompt(`Ingrese el nombre del finalista ${i+1}`));
+    marcas.push(Number(prompt(`Ingrese la marca del finalista ${i+1} en metros`)));
+}
+let mayorMarca = Math.max(...marcas);
+let indexMayor = marcas.indexOf(mayorMarca);
+const record = 15.5;
+if (mayorMarca > record) {
+    alert(`!El finalista ganador de la medalla de oro es ${nombres[indexMayor]} y ademas rompió el record de 15,5 metros con una marca de ${marcas[indexMayor]} metros por lo cual se le realizará un pago de 500 millones!`);
+}
+else {
+    alert(`El finalista ganador de la medalla de oro es ${nombres[indexMayor]} con una marca de ${mayorMarca} metros`);
+}
