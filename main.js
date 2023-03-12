74afbd6348723251d9338c19f94c42a37dafa9b4
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-03-11 20:38:36 -0500

    feat: :sparkles: Ejercicio 10

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-let finalistas = 3;
-let nombres = [];
-let marcas = [];
+let valores = [];
+let respuesta = Number;
+let indice = 0;
+let sumatoria = 0;
 
-for (let i = 0; i < finalistas; i++) {
-    nombres.push(prompt(`Ingrese el nombre del finalista ${i+1}`));
-    marcas.push(Number(prompt(`Ingrese la marca del finalista ${i+1} en metros`)));
-}
-let mayorMarca = Math.max(...marcas);
-let indexMayor = marcas.indexOf(mayorMarca);
-const record = 15.5;
-if (mayorMarca > record) {
-    alert(`!El finalista ganador de la medalla de oro es ${nombres[indexMayor]} y ademas rompió el record de 15,5 metros con una marca de ${marcas[indexMayor]} metros por lo cual se le realizará un pago de 500 millones!`);
-}
-else {
-    alert(`El finalista ganador de la medalla de oro es ${nombres[indexMayor]} con una marca de ${mayorMarca} metros`);
-}
+do {
+    valores.push(Number(prompt("Ingrese un dato numerico")));
+    respuesta = Number(prompt("Ingrese 0 para parar de ingresar numeros"));
+    sumatoria += valores[indice];
+    indice++;
+} while (respuesta != 0);
+
+let promedio = (sumatoria / indice);
+let totalDeDatos = indice;
+let mayorDato = Math.max(...valores);
+let menorDato = Math.min(...valores);
+
+alert(`La sumatoria de los datos es de ${sumatoria}\n
+El promedio de los datos es de ${promedio}\n
+En total se ingresaron ${totalDeDatos} datos\n
+El dato con mayor valor ingresado es ${mayorDato}\n
+El dato con menor valor ingresado es ${menorDato}`);
